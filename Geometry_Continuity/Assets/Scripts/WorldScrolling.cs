03ba244e5b3dfcee0f28d381749284ced8a7b35d
Apex1429 <apex1429@gmail.com>
2023-07-14 13:12:14 -0400

    Minor changes

--- Geometry_Continuity/Assets/Scripts/WorldScrolling.cs
+++ Geometry_Continuity/Assets/Scripts/WorldScrolling.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class WorldScrolling : MonoBehaviour
 {
+    [SerializeField] Transform playerTransform;
+    Vector2Int currentTilePosition;
+    GameObject[,] terrainTiles;
 
+    [SerializeField] int TerrainTileHorizontalCount;
+    [SerializeField] int TerrainTileVerticalCount;
+
+    private void Awake()
+    {
+        
+        terrainTiles = new GameObject[TerrainTileHorizontalCount, TerrainTileVerticalCount];
+             
+    }
 }
